b3603a0319c0e87d455f7a9dd4c6aec8b09631f6
荣顶 <wangrongding@qq.com>
2023-05-05 15:19:28 +0800

    feat: 完善首页样式

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,20 +26,29 @@ export default function Home({ countImg, imgList }: Props) {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/wangrongding/access-counter"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/github.svg"
+              alt="Github Logo"
               className="dark:invert"
-              width={100}
-              height={24}
+              width={20}
+              height={20}
               priority
             />
-            <h2 className={`text-xl font-semibold`}>Deploy-&gt;</h2>
+            <h2 className={""}>source code</h2>
           </a>
+
+          {/* <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={`text-xl font-semibold`}>Deploy-&gt;</h2>
+          </a> */}
         </div>
       </div>
 
