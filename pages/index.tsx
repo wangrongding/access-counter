062f792edadda37fbd307b64d5ee6e8ad99ff99b
荣顶 <wangrongding@qq.com>
2023-05-05 14:00:58 +0800

    feat: 完善首页样式

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ interface Props {
 
 export default function Home({ countImg }: Props) {
   const [text, setText] = useState("");
+  const baseUrl = "access-counter.vercel.app/api/counter?name=github-username";
 
   return (
     <main
@@ -19,19 +20,15 @@ export default function Home({ countImg }: Props) {
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          地址栏输入：
-          <code>
-            access-counter.vercel.app/api/counter?name=github-username
-          </code>
+          GitHub profile 访问量统计
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -40,6 +37,7 @@ export default function Home({ countImg }: Props) {
               height={24}
               priority
             />
+            <h2 className={`text-xl font-semibold`}>Deploy-&gt;</h2>
           </a>
         </div>
       </div>
@@ -55,29 +53,29 @@ export default function Home({ countImg }: Props) {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          地址栏输入：
-          <code>
-            access-counter.vercel.app/api/counter?name=github-username
-          </code>
+      <div className="mb-32 lg:text-left flex gap-4 flex-col">
+        <h2 className=" font-bold text-xl">How to use:</h2>
+        <p>
+          <span className="font-bold mr-4 w-[150px] inline-block">SVG address:</span>
+          <span className=" bg-slate-300 rounded p-2 w-[700px] inline-block ">
+            {` ${baseUrl}?name=github-username`}
+          </span>
         </p>
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <p>
+          <span className="font-bold mr-4 w-[150px] inline-block">Img tag:</span>
+          <span className=" bg-slate-300 rounded p-2 w-[700px] inline-block ">
+            {` <img src="${baseUrl}" />`}
+          </span>
+        </p>
+        <p>
+          <span className="font-bold mr-4 w-[150px] inline-block">Markdown:</span>
+          <span className=" bg-slate-300 rounded p-2 w-[700px] inline-block ">
+            {`![](${baseUrl})`}
+          </span>
+        </p>
+        <p className="bg-amber-200 rounded px-2">
+          Data can access by anyone, please DO NOT enter personal information
+        </p>
       </div>
     </main>
   );
